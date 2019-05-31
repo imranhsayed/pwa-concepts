ecfde24ea4c870d3d26e64dc22dd754a35099255
Imran Sayed <imranhsayed@gmail.com>
2019-06-01 01:10:13 +0530

    Register service worker in main.js and create a service worker file sw.js in root dir

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ function toggleNav(){
     }
     hidden = !hidden;
 }
+
+// Check if the serviceWorker Object exists in the navigator object ( means if browser supports SW )
+if ( 'serviceWorker' in navigator ) {
+
+	/**
+	 * Register Service Worker
+	 * 'sw.js' is our service worker file
+	 */
+	navigator.serviceWorker.register( '/sw.js' )
+		.then( ( res ) => {
+			console.warn( `Sevice Worker Registered ${res.scope}` );
+		} )
+		.catch( err => console.warn( 'SW registration failed' + err ) )
+
+} else {
+	console.warn( 'Service Workers not supported' );
+}
